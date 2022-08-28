03e3a356fbcaac0293d0a6f363d95ffb6fda6c5d
rpturbina <rizki.pratama.turbina@gmail.com>
2022-08-28 09:13:25 +0700

    Update to new design

---
+++ assets/js/constant.js
@@ -0,0 +1,77 @@
+export const WEBSITE_IDENTITY = {
+    author: 'rpturbina',
+    title: 'rpturbina | Personal Website',
+    copyright: `© ${new Date().getFullYear()} by Rizki Pratama Turbina. All rights reserved.`,
+};
+
+// Nav items
+export const NAV_ITEMS = [
+    {
+        label: 'Home',
+        href: '#home',
+    },
+    {
+        label: 'Portofolio',
+        href: '#portofolio',
+    },
+    {
+        label: 'Contact',
+        href: '#contact',
+    },
+    {
+        label: 'About',
+        href: '#about',
+    },
+];
+
+// Contact items
+export const CONTACT_ITEMS = [
+    {
+        label: 'Github Icon',
+        iconSrc: 'assets/images/github-icon.svg',
+        href: 'https://github.com/rpturbina',
+    },
+    {
+        label: 'LinkedIn Icon',
+        iconSrc: 'assets/images/linkedin-icon.svg',
+        href: 'https://www.linkedin.com/in/rpturbina/',
+    },
+    {
+        label: 'Instagram Icon',
+        iconSrc: 'assets/images/instagram-icon.svg',
+        href: 'https://www.instagram.com/rpturbina/',
+    },
+    {
+        label: 'Email Icon',
+        iconSrc: 'assets/images/email-icon.svg',
+        href: 'mailto:rizki.pratama.turbina@gmail.com',
+    },
+];
+
+// Project items
+export const PROJECT_ITEMS = [
+    {
+        title: 'Tribute Page - B. J. Habibie',
+        description:
+            "I built this page for tribute and sharing about B. J. Habibie's life timeline and to complete the freeCodeCamp course about Responsive Web Design.",
+        stack: 'HTML, CSS',
+        'project-repo': 'https://github.com/rpturbina/fcc-tribute-page',
+        'project-demo': 'https://rpturbina.github.io/fcc-tribute-page',
+    },
+    {
+        title: 'Survey Form - freeCodeCamp',
+        description:
+            'I built this survey form for completing the freeCodeCamp course about Responsive Web Design.',
+        stack: 'HTML, CSS, JavaScript',
+        'project-repo': 'https://rpturbina.github.io/fcc-survey-form',
+        'project-demo': 'https://github.com/rpturbina/fcc-survey-form',
+    },
+    {
+        title: 'JavaScript Documentation Page',
+        description:
+            'I built this technical documentation page for completing the freeCodeCamp course about Responsive Web Design.',
+        stack: 'HTML, CSS, JavaScript',
+        'project-repo': 'https://github.com/rpturbina/fcc-technical-doc/',
+        'project-demo': 'https://rpturbina.github.io/fcc-technical-doc/',
+    },
+];
